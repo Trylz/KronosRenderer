4aa61f893e765c17f6b9ebb8b9219725d908b655
Trylz <trylzengine@outlook.com>
2017-12-18 12:57:51 -0500

    Update rights

--- OrionApplication.cpp
+++ OrionApplication.cpp
@@ -1,5 +1,5 @@
 //==========================================================================================
-// Copyright (c) Yann Clotioloman Yeo, 2017 - All Rights Reserved
+// Copyright (c) Yann Clotioloman Yeo, 2017
 //
 //	Author					: Yann Clotioloman Yeo
 //	E-Mail					: orionrenderer@gmail.com
