f12df821171607ce1d8f047319e59c19a960c876
Trylz <trylzengine@outlook.com>
2018-01-16 01:44:25 -0500

    Update header

--- Core/Graphics/Renderer/Realtime/Dx12/Effect/BaseEffect.h
+++ Core/Graphics/Renderer/Realtime/Dx12/Effect/BaseEffect.h
@@ -1,7 +1,5 @@
 //========================================================================
-// Copyright (c) Yann Clotioloman Yeo, 2017 - All Rights Reserved
-// Unauthorized copying of this file, via any medium is strictly prohibited
-// Proprietary and confidential
+// Copyright (c) Yann Clotioloman Yeo, 2017
 //
 //	Author					: Yann Clotioloman Yeo
 //	E-Mail					: orionrenderer@gmail.com
